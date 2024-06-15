b3a6bb4713a26c2396da1c110394a28f3b54e740
Marina-mv <marina.melnik.0202@mail.ru>
2024-06-15 15:54:08 +0300

    connecting back and front

--- lessons/admin.py
+++ lessons/admin.py
@@ -5,12 +5,18 @@ from lessons.models import Course, Lesson, Task, TaskSubmission
 
 @admin.register(Course) # регистрирация модели Course в административной панели
 class CourseAdmin(admin.ModelAdmin):
-    pass
+    list_display=['title'] #отображение названия курса в списке моделей в административной панели
 
 
 @admin.register(Lesson) # регистрирация модели Lesson в административной панели
 class LessonAdmin(admin.ModelAdmin):
-    list_display = ('title', 'course') # отображение названия урока и курса в списке уроков в админ. панели 
+    list_display = ['title', 'get_course_title'] #отображение названия урока и названия курса, к которому он привязан
+    def get_course_title(self, obj): #возвращает название курса для урока
+        if obj.course:
+            return obj.course.title
+        else:
+            return '-'  # Если курс не указан
+    get_course_title.short_description = 'Course'  # Название колонки в административной панели
 
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
--- lessons/views.py
+++ lessons/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse
+from lessons.models import Course, Lesson
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+def courses(request):
+    courses = Course.objects.all()
+    return JsonResponse({
+        'courses': [
+            {'id': course.id, 'title': course.title} for course in courses
+        ]
+    })
+
+@csrf_exempt
+def lessons(request, course_id):
+    course = Course.objects.get(id=course_id)
+    lessons = Lesson.objects.filter(course=course)
+    return JsonResponse({
+            'lessons': [
+                {'id': lesson.id, 'title': lesson.title} for lesson in lessons
+            ]
+        })
